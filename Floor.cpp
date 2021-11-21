49faa5df505a28a760802335dcf6f98e34a80bab
jonahleibowitzzz <93497598+jonahleibowitzzz@users.noreply.github.com>
2021-11-21 13:12:03 -0500

    finished floor?

--- Floor.cpp
+++ Floor.cpp
@@ -18,7 +18,7 @@ int Floor::tick(int currentTime) {
 }
 
 void Floor::addPerson(Person p, int request) {
-if(numPeople < MAX_PEOPLE_PER_FLOOR){
+    if(numPeople < MAX_PEOPLE_PER_FLOOR){
         people[numPeople] = p;
         numPeople++;
     }
@@ -32,23 +32,64 @@ if(numPeople < MAX_PEOPLE_PER_FLOOR){
 }
 
 void Floor::removePeople(int indicesToRemove[MAX_PEOPLE_PER_FLOOR], int numPeopleToRemove) {
-    //TODO: Implement removePeople
+    /*
+     What we need to do:
+     Given:
+     Array of people objects
+     Number index for each person
+     List of indices to be removed (not always in order)
+     
+     Remove each of the indicated people
+     Close list so there is no white space
+     
+     Plan:
+     SECOND SOLUTION
+     For loop the number of times of numPeopleToRemove
+     Erase single person, back the remaining items up
+        Create for loop starting at index to remove, equals everything back one index. Last index set to 0
+        
+     
+     Update numPeople
+     Reassess up/down requests at the end using function
+     
+     Tester:
+     cout << "ORIGINAL:" << endl;
+     for (int i = 0; i < MAX_PEOPLE_PER_FLOOR; i++) {
+         cout << "Person " << i << " turn: " << people[i].getTurn() << endl;
+     }
+     */
+    
+    
+    ///Blank all indicated indices
+    Person blank;
+    for(int i = 0; i < numPeopleToRemove; i++){
+        people[indicesToRemove[i]] = blank;
+    }
+    
+    ///USE THIS TO BACK EVERYTHING UP SO THERE'S NO HOLES
+    for(int k = 0; k < numPeopleToRemove; k ++){
+        for(int i = 0; i < numPeople; i++){
+            if(people[i].getTurn() == 0 && people[i].getAngerLevel() == 0 &&
+               people[i].getTargetFloor() == 0 && people[i].getCurrentFloor() == 0){
+                for(int j = i; j < numPeople; j++){ //ISSUES WHEN ALL 10 ARE FILLED?
+                    people[j] = people[j + 1];
+                }
+            }
+        }
+    }
+    numPeople -= numPeopleToRemove;
+    resetRequests();
 }
 
 void Floor::resetRequests() {
+    hasUpRequest = false;
+    hasDownRequest = false;
     for(int i = 0; i < numPeople; i++){
         if (getPersonByIndex(i).getTargetFloor() > getPersonByIndex(i).getCurrentFloor()) {
             hasUpRequest = true;
         }
-        else{
-            hasUpRequest = false;
-        }
-        
         if (getPersonByIndex(i).getTargetFloor() < getPersonByIndex(i).getCurrentFloor()){
             hasDownRequest = true;
-        }
-        else{
-            hasDownRequest = false;
         }
     }
 }
