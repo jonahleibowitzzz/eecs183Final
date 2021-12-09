09d590fd9a77d1538c535df722cd956cf418445e
jonahleibowitzzz <93497598+jonahleibowitzzz@users.noreply.github.com>
2021-12-08 20:17:05 -0500

    Create AI.cpp

---
+++ AI.cpp
@@ -0,0 +1,14 @@
+#include "AI.h"
+#include <cassert>
+
+// This file is used only in the Reach, not the Core.
+// You do not need to make any changes to this file for the Core
+
+string getAIMoveString(const BuildingState& buildingState) {
+    
+    return "";
+}
+
+string getAIPickupList(const Move& move, const BuildingState& buildingState, const Floor& floorToPickup) {
+    return "";
+}
